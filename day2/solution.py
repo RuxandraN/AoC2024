131182680ea435f659eafd723f29a0d12e56625d
Ruxandra <ruxandra.nanu@gmail.com>
2024-12-04 14:49:08 +0200

    Style refactoring

--- day2/solution.py
+++ day2/solution.py
@@ -3,9 +3,10 @@ from typing import Final
 SAFE_LEVELS_DIFFS: Final = frozenset({1, 2, 3})
 
 def get_input():
-    with open('input.txt') as file:
-        return([int(num) for num in line.split()] for line in file.read().splitlines())  
-    
+    with open("input.txt") as file:
+        return([int(num) for num in line.split()] for line in file.read().splitlines())
+
+
 def part_1():
     safe_reports = 0
     reports = get_input()
@@ -29,34 +30,38 @@ def is_safe(report):
         new_order = 'asc' if diff < 0 else 'dsc'
         
         if order is None:
-            order = new_order                         
+            order = new_order
         elif order != new_order:
             return index
         
     return -1
-    
+
     
 def part_2():
+    """Report is safe or becomes safe if one level is removed.
+    
+    If the unsafe level is the last one then it's safe.
+    
+    If the unsafe level is the second one, checks if report is safe without the first level.
+    
+    Otherwise checks if report is safe without current or next unsafe level.
+    """
+
     safe_reports = 0
     reports = get_input()
     
     for report in reports:
         unsafe_index = is_safe(report)
                    
-        """Report is safe or becomes safe if one level is removed.
-        
-        If the unsafe level is the last one then it's safe.
-        
-        If the unsafe level is the second one, checks if report is safe without the first level.
-        
-        Otherwise checks if report is safe without current or next unsafe level.
-        """
-        if unsafe_index in [-1, len(report) - 2] \
-            or unsafe_index == 1 and is_safe(report[unsafe_index:]) == -1 \
-            or is_safe(report[:unsafe_index] + report[unsafe_index+1:]) == -1 \
-            or is_safe(report[:unsafe_index+1] + report[unsafe_index+2:]) == -1:
+
+        if (
+            unsafe_index in [-1, len(report) - 2]
+            or unsafe_index == 1 and is_safe(report[unsafe_index:]) == -1
+            or is_safe(report[:unsafe_index] + report[unsafe_index+1:]) == -1
+            or is_safe(report[:unsafe_index+1] + report[unsafe_index+2:]) == -1
+        ):
             safe_reports += 1
-    
+            
     return f"total: {safe_reports}"
 
 print(part_1())
